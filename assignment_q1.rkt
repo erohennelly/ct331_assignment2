89d304e35bd0269249fbcb1008978c101441adfc
erohennelly <erohennelly@hotmail.com>
2017-10-25 18:21:23 +0100

    Add files via upload

--- assignment_q1.rkt
+++ assignment_q1.rkt
@@ -1,1 +1,6 @@
 #lang racket
+(cons 1 2)
+(cons 1(cons 2(cons 3 empty)))
+(cons "hello"(cons 20(cons(cons 1(cons 2(cons 3 empty)))empty)))
+(list "hello" 20(list 1 2 3))
+(append '("hello") '(20) '((1 2 3)))
